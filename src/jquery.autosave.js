f6161c093a071b716446398d4dc561fb32d1beb1
Kelatev <kelatev@gmail.com>
2017-02-06 12:00:29 +0200

    bugfix

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -1,80 +1,91 @@
-$(function () {
-    $(".autosave-form").each(function () {
-        var self = $(this),
-            formId = self.attr("data-autosave"),
-            storageName = "autosave";
+var Autosave = Autosave || {};
+Autosave.storageName = 'autosave';
 
-        var _setItem = function (that) {
-            var items = _getFullItem(),
-                found = false,
-                index = $(that).index(),
-                text = that.value;
+Autosave._setItem = function (that) {
+    var items = Autosave._getFullItem(),
+        formId = $(that).closest('form').attr('data-autosave'),
+        index = $(that).index(),
+        found = false,
+        text = that.value;
 
-            $.each(items, function (key, value) {
-                if (value.id == formId && value.i == index) {
-                    value.text = text;
-                    value.reset = false;
-                    found = true;
-                }
-            });
-
-            if (!found) {
-                items.push({"id": formId, "i": index, "text": text, "reset": false});
-            }
-
-            window.localStorage.setItem(storageName, JSON.stringify(items));
-        };
-
-
-        var _getFullItem = function () {
-            return JSON.parse(window.localStorage.getItem(storageName)) || [];
-        };
-
-
-        var _getText = function (t, reset) {
-            var index = $(t).index(),
-                text = "";
-
-            $.each(_getFullItem(), function (key, value) {
-                if (value.id == formId && value.i == index && ((reset && value.reset == false) || !reset)) {
-                    text = value.text;
-                    return;
-                }
-            });
-
-            return text;
-        };
-
-
-        //restore
-        self.find('textarea').each(function () {
-            if (this.value == "") {
-                this.value = _getText(this, true);
-            }
-        });
-
-
-        self.find('textarea').on('keydown', function (e) {
-            if (e.keyCode == 90 && e.ctrlKey) {
-                if (this.value == "") {
-                    this.value = _getText(this, false);
-                    _setItem(this);
-                }
-            } else if (!e.ctrlKey) {
-                _setItem(this);
-            }
-        });
-
-
-        self.on('reset', function () {
-            var json = _getFullItem();
-            $.each(json, function (key, value) {
-                if (value.id == formId) {
-                    value.reset = true;
-                }
-            });
-            window.localStorage.setItem(storageName, JSON.stringify(json));
-        });
-
+    $.each(items, function (key, value) {
+        if (value.id == formId && value.i == index) {
+            value.text = text;
+            value.reset = false;
+            found = true;
+        }
     });
+
+    if (!found) {
+        items.push({"id": formId, "i": index, "text": text, "reset": false});
+    }
+
+    window.localStorage.setItem(Autosave.storageName, JSON.stringify(items));
+};
+Autosave._getFullItem = function () {
+    return JSON.parse(window.localStorage.getItem(Autosave.storageName)) || [];
+};
+Autosave._getText = function (that, reset) {
+    var formId = $(that).closest('form').attr('data-autosave'),
+        index = $(that).index(),
+        text = '';
+
+    $.each(Autosave._getFullItem(), function (key, value) {
+        if (value.id == formId && value.i == index && ((reset && value.reset == false) || !reset)) {
+            text = value.text;
+            return;
+        }
+    });
+
+    return text;
+};
+
+
+Autosave.bind = function (e) {
+    if (e.keyCode == 90 && e.ctrlKey) {
+        if (this.value == "") {
+            this.value = Autosave._getText(this, false);
+            Autosave._setItem(this);
+        } else {
+            return true;
+        }
+    } else if (!e.ctrlKey) {
+        Autosave._setItem(this);
+    }
+};
+Autosave.restore = function () {
+    if (this.value == '') {
+        this.value = Autosave._getText(this, true);
+    }
+};
+Autosave.reset = function () {
+    var json = Autosave._getFullItem(),
+        formId = $(this).closest('form').attr('data-autosave');
+    $.each(json, function (key, value) {
+        if (value.id == formId) {
+            value.reset = true;
+        }
+    });
+    window.localStorage.setItem(Autosave.storageName, JSON.stringify(json));
+};
+Autosave.remove = function (that) {
+    var json = Autosave._getFullItem(),
+        formId = $(that).attr('data-autosave');
+    $.each(json, function (key, value) {
+        if (value.id == formId) {
+            var index = json.indexOf(value);
+            if (index > -1) {
+                json.splice(index, 1);
+            }
+        }
+    });
+    window.localStorage.setItem(Autosave.storageName, JSON.stringify(json));
+};
+
+
+$(document).on('keyup', '.autosave-form textarea', Autosave.bind);
+$(document).on('reset', '.autosave-form', Autosave.reset);
+$(function () {
+    $('.autosave-form textarea').each(Autosave.restore);
 });
+
